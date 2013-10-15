741a11df46414dc5905ab83c3ad9a8d27dc72935
Ruofan <rfan.kd@gmail.com>
2013-10-15 19:53:42 -0400

    getUnit test for the individual temperatures: Kelvin, Celsius and Fahrenheit

--- TemperatureTest.java
+++ TemperatureTest.java
@@ -11,15 +11,24 @@ public class TemperatureTest {
 
   // Add multiple tests to check all functions of
   // {@Code Temperature} class.
-
-  // @Test
+  
+	
+   @Test
    public void test_Kelvin (){
       Temperature kelvinTemperature = new Temperature (0,Temperature.units.KELVIN);
-      assertEquals(Temperature.Units.KELVIN,tInKelvin.getUnits());
+      assertEquals(Temperature.Units.KELVIN,kelvinTemperature.getUnits());
    }
 
-  // @Test
-  // public void .... (){
-  //    ...
-  // }
+   @Test
+   public void getUnitsTestForCELSIUS(){
+ 	  Temperature celciusTemperature = new Temperature(0,Temperature.Units.CELSIUS);
+ 	  assertEquals(Temperature.Units.CELSIUS, celciusTemperature.getUnits() );
+   }
+   
+   public void getUnitsTestForFAHRENHEIT(){
+		  Temperature fahrenheitTemperature = new Temperature(0,Temperature.Units.FAHRENHEIT);
+		  assertEquals(Temperature.Units.FAHRENHEIT, fahrenheitTemperature.getUnits());
+	  }
+	 
+   
 }
